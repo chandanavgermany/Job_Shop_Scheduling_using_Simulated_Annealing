b39f6354bdf6605539d12d763ec76f0fa849aefb
Chandan A V <mrchandanav@gmail.com>
2019-08-06 18:15:37 +0200

    Update jss_sa.py

--- jss_sa.py
+++ jss_sa.py
@@ -10,11 +10,16 @@ from Utilities import Utilities
 from SimulatedAnnealing import SimulatedAnnealing
 import matplotlib.pyplot as plt
 
-
+# Initialization of the objects
 jobshopLoader = JobShopLoader()
-jobs = jobshopLoader.load("data.txt")
 utilitiesObj = Utilities()
-
 simulatedAnnealing = SimulatedAnnealing()
+
+# Loading the data from the input file
+jobs = jobshopLoader.load("data.txt")
+
+# Calling the search algorithm to find the best solution for scheduling the jobs on machines
 cost, solution = simulatedAnnealing.search(jobshopLoader, utilitiesObj, maxTime=20, T=200, termination=10, halting=10, mode='random',shrink = 0.8)
+
+# Display the solution
 utilitiesObj.showSolution(jobshopLoader, solution)
